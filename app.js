55cbbe293be1f0d7a27e426e7f6ef6160911f10f
Shahjaman1 <shah.jaman58@gmail.com>
2024-11-10 23:31:19 +0530

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,27 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+
+//new edit
+const ejs = require('ejs');
+const fs = require('fs');
+const path = require('path');
+
+const renderEJS = (templatePath, outputPath, data) => {
+    ejs.renderFile(templatePath, data, {}, (err, str) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        fs.writeFileSync(outputPath, str);
+    });
+};
+
+const data = { title: 'My Portfolio', projects: [] }; // Your data here
+const templatePath = path.join(__dirname, 'views', 'index.ejs');
+const outputPath = path.join(__dirname, 'public', 'index.html');
+
+renderEJS(templatePath, outputPath, data);
 const users = [{
     userName: "Aditya Gupta",
     userEmail: "aditya@gmail.com",
